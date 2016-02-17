7196793dc1f6d1723a870485f80163b220a259f4
nath@home <nathdwek@ulb.ac.be>
2016-02-18 00:20:47 +0100

    REFACTOR+COMMENTS. ALSO LAZY. BUT GOOD AT ASM FFS

--- risc16/ex1_9.asm
+++ risc16/ex1_9.asm
@@ -2,97 +2,96 @@
 
 	movi	2, 0x88dc
 
-			//Reasoning:
-			//Goal: e=a*b
-			//init: c = 0x0001 running mask
-			//loop 16 times: (until mask = 0x0000)
-			//d = a masked by c
-			//if d!=0{
-			//e = e + b
-			//}
-			//b = b + b
-			//shift mask c
-			//end loop
+//Reasoning:
+//Goal: r=a*b
+//init: c = 0x0001 running mask
+//loop 16 times: (until mask = 0x0000)
+//d = a masked by c
+//if d!=0{
+//r = r + b
+//}
+//b = b + b
+//shift mask c
+//end loop
 
-	add	5, 0, 2
-	add	6, 0, 0
+//b [2,5] and r [3,4] (spec) are always in registers!
 
-	addi	2, 0, 1	//Running mask c
+	addi	6, 0, 1		//Running mask c
 
-	add	3, 0, 0
-	add	4, 0, 0
-
-loop:	nand	7, 2, 1
+loop:	nand	7, 6, 1
 	nand	7, 7, 7
-	sw	1, 0, 0
-	sw	2, 0, 1
+	sw	6, 0, 0
+	sw	1, 0, 1
 
-	sw	5, 0, 4
-	sw	6, 0, 5
-
-	beq	7, 0, inter1
-
-	movi	1, 0x8000	//Keep msb
-	nand 	1, 3, 1
-	nand 	1, 1, 1
-	beq 	1, 0, zero
-	addi 	7, 0, 1
+	beq	7, 0, shftb1	//nth bit of a = 0 => dont do r = r + b
+				//Else do it: here is a 32 + 32 bit addition
+				// [2,5] + [3,4] = [3,4] and keep [2,5] intact
+	movi	1, 0x8000	//Keep MSB
+	nand 	6, 2, 1
+	nand 	6, 6, 6
+	beq 	6, 0, nomsb 	//First MSB is 0 initialize carry to 0
+				//and go to second MSB
+	addi 	7, 0, 1		//Else initialize carry to 1 and go to second msb
  	beq 	0, 0, msb2
-zero: 	addi 	7, 0, 0
+nomsb: 	addi 	7, 0, 0
 
-msb2:	movi	1, 0x8000	//Keep msb
-	nand 	1, 5, 1
-	nand 	1, 1, 1
-	beq 	1, 0, ad15
+msb2:	nand 	6, 3, 1		//Keep MSB of 15LSBs of second operand
+	nand 	6, 6, 6
+	beq 	6, 0, ad15	//Other MSB is 0 go straight to 15 bits addition
 	addi 	7, 7, 1
 
 ad15:	movi	1, 0x7FFF	//Keep 15LSBs
+	nand	6, 1, 2
+	nand	6, 6, 6
 	nand	3, 1, 3
 	nand	3, 3, 3
-	nand	5, 1, 5
-	nand	5, 5, 5
-	add	3, 3, 5		//15bits addition
+	add	3, 3, 6		//15bits addition
 
-	movi	1, 0x8000	//Keep msb
+	movi	1, 0x8000	//Keep msb of result
 	nand 	1, 3, 1
 	nand 	1, 1, 1
-	beq 	1, 0, adcy1
-	addi 	7, 7, 1
-	beq	0, 0, adcy1
+	beq 	1, 0, adcy1	//MSB of result = 0 go to add LSB of carry to result
+	addi 	7, 7, 1		//Else, add 1 to carry
+	beq	0, 0, adcy1	//Then add LSB of carry to result
 
-inter4:	beq	0, 0, loop
+loop2:	beq	0, 0, loop
 
 adcy1:	movi	1, 0x7FFF	//Keep 15LSBs of 15 bits sum
 	nand	3, 3, 1
 	nand	3, 3, 3
-	movi	1, 0x0001	//Keep LSB of Cy
-	nand	2, 1, 7
-	nand	2, 2, 2
-	beq	2, 0, adcy2
-	movi	1, 0x8000	//Add MSB=1
+	addi	1, 0, 1		//Keep LSB of Cy
+	nand	1, 1, 7
+	nand	1, 1, 1
+	beq	1, 0, adcy2	//LSB of Cy is 0 go look MSB of carry.
+	movi	1, 0x8000	//Else add MSB = 1 to result of 15bits addition
 	add	3, 1, 3
 
-adcy2:	movi	1, 0x0002	//Keep 2nd bit of Cy
-	nand	2, 1, 7
-	nand	2, 2, 2
-	beq	2, 0, nocy
-	addi	5, 0, 1
-	beq	0, 0, high
-nocy:	addi	5, 0, 0
+adcy2:	addi	1, 0, 2		//Keep 2nd bit of Cy
+	nand	1, 1, 7
+	nand	1, 1, 1
+	beq	1, 0, nocy	//No carry from low weight addition to the
+				//result of the high weight addition
+				//=> initialize the high weight addition result
+				//to 0 and proceed. Else initialize it to 1 and
+				//proceed
+	addi	6, 0, 1
+	beq	0, 0, high	//Go to high bits addition
+nocy:	addi	6, 0, 0
 
 high:	movi	1, 0x8000	//Keep msb
-	nand 	1, 4, 1
+	nand 	1, 5, 1		//From here on it's the same structure as the
+				//The previous addition (same labels suffixed by h)
 	nand 	1, 1, 1
-	beq 	1, 0, zeroh
+	beq 	1, 0, nmsbh
 	addi 	7, 0, 1
  	beq 	0, 0, msb2h
-zeroh: 	addi 	7, 0, 0
+nmsbh: 	addi 	7, 0, 0
 	beq	0, 0, msb2h
 
-inter1:	beq	0, 0, noad
+shftb1:	beq	0, 0, shftb
 
 msb2h:	movi	1, 0x8000	//Keep msb
-	nand 	1, 6, 1
+	nand 	1, 4, 1
 	nand 	1, 1, 1
 	beq 	1, 0, ad15h
 	addi 	7, 7, 1
@@ -100,10 +99,10 @@ msb2h:	movi	1, 0x8000	//Keep msb
 ad15h:	movi	1, 0x7FFF	//Keep 15LSBs
 	nand	4, 1, 4
 	nand	4, 4, 4
-	nand	6, 1, 6
-	nand	6, 6, 6
-	add	4, 4, 6		//15bits addition
-	add	4, 4, 5
+	nand	1, 1, 5
+	nand	1, 1, 1
+	add	4, 4, 6		//high 15 bits of first operand + carry
+	add	4, 4, 1		//+high 15bits of second operand
 
 	movi	1, 0x8000	//Keep msb
 	nand 	1, 4, 1
@@ -112,124 +111,34 @@ ad15h:	movi	1, 0x7FFF	//Keep 15LSBs
 	addi 	7, 7, 1
 	beq	0, 0, adcy1h
 
-inter3:	beq	0, 0, inter4
+loop1:	beq	0, 0, loop2
 
-adcy1h:	movi	1, 0x7FFF	//Keep 15LSBs of 15 bits sum
+adcy1h:	movi	1, 0x7FFF	//Keep 15LSBs of 15 bits sum of the high bits
 	nand	4, 1, 4
 	nand	4, 4, 4
-	movi	1, 0x0001	//Keep LSB of Cy
-	nand	2, 1, 7
-	nand	2, 2, 2
-	beq	2, 0, storee
-	movi	1, 0x8000	//Add MSB=1
-	add	4, 1, 4
-
-storee:	sw	3, 0, 2		//Finished e = e + b. Store results
-	sw	4, 0, 3
-
-noad:	lw	5, 0, 4
-	lw	6, 0, 5
-
-	add	3, 0, 5		//Move b (5,6) to (3,4) in order to compute b = b + b
-	add	4, 0, 6
-				//start b =  b+ b
-	movi	1, 0x8000	//Keep msb
-	nand 	1, 3, 1
-	nand 	1, 1, 1
-	beq 	1, 0, zero_
-	addi 	7, 0, 1
- 	beq 	0, 0, msb2_
-zero_: 	addi 	7, 0, 0
-
-msb2_:	movi	1, 0x8000	//Keep msb
-	nand 	1, 5, 1
-	nand 	1, 1, 1
-	beq 	1, 0, ad15_
-	addi 	7, 7, 1
-
-ad15_:	movi	1, 0x7FFF	//Keep 15LSBs
-	nand	3, 1, 3
-	nand	3, 3, 3
-	nand	5, 1, 5
-	nand	5, 5, 5
-	add	3, 3, 5		//15bits addition
-
-	movi	1, 0x8000	//Keep msb
-	nand 	1, 3, 1
-	nand 	1, 1, 1
-	beq 	1, 0, adcy1_
-	addi 	7, 7, 1
-
-adcy1_:	movi	1, 0x7FFF	//Keep 15LSBs of 15 bits sum
-	nand	3, 3, 1
-	nand	3, 3, 3
-	movi	1, 0x0001	//Keep LSB of Cy
-	nand	2, 1, 7
-	nand	2, 2, 2
-	beq	2, 0, adcy2_
-	movi	1, 0x8000	//Add MSB=1
-	add	3, 1, 3
-	beq	0, 0, adcy2_
-
-inter2:	beq	0, 0, inter3
-
-adcy2_:	movi	1, 0x0002	//Keep 2nd bit of Cy
-	nand	2, 1, 7
-	nand	2, 2, 2
-	beq	2, 0, nocy_
-	addi	5, 0, 1
-	beq	0, 0, high_
-nocy_:	addi	5, 0, 0
-
-high_:	movi	1, 0x8000	//Keep msb
-	nand 	1, 4, 1
-	nand 	1, 1, 1
-	beq 	1, 0, zroh_
-	addi 	7, 0, 1
- 	beq 	0, 0, msb2h_
-zroh_: 	addi 	7, 0, 0
-
-msb2h_:	movi	1, 0x8000	//Keep msb
-	nand 	1, 6, 1
-	nand 	1, 1, 1
-	beq 	1, 0, ad15h_
-	addi 	7, 7, 1
-
-ad15h_:	movi	1, 0x7FFF	//Keep 15LSBs
-	nand	4, 1, 4
-	nand	4, 4, 4
-	nand	6, 1, 6
+	addi	1, 0, 1		//Keep LSB of Cy
+	nand	6, 1, 7
 	nand	6, 6, 6
-	add	4, 4, 6		//15bits addition
-	add	4, 4, 5
-
-	movi	1, 0x8000	//Keep msb
-	nand 	1, 4, 1
-	nand 	1, 1, 1
-	beq 	1, 0, adc1h_
-	addi 	7, 7, 1
-
-adc1h_:	movi	1, 0x7FFF	//Keep 15LSBs of 15 bits sum
-	nand	4, 1, 4
-	nand	4, 4, 4
-	movi	1, 0x0001	//Keep LSB of Cy
-	nand	2, 1, 7
-	nand	2, 2, 2
-	beq	2, 0, storeb
-	movi	1, 0x8000	//Add MSB=1
+	beq	6, 0, shftb
+	movi	1, 0x8000	//Add MSB=1 to high bits of result
 	add	4, 1, 4
 
-storeb:	add	5, 3, 0
-	add	6, 4, 0
+shftb:	movi	1, 0x8000
+	nand 	1, 2, 1
+	nand 	1, 1, 1
+	beq 	1, 0, shft0
+	addi 	7, 0, 1
+ 	beq 	0, 0, shift
+shft0: 	addi 	7, 0, 0
+shift:  add     2, 2, 2
+	add     5, 5, 5
+	add     5, 5, 7
 
-	lw	1, 0, 0
+	lw	1, 0, 1
 
-	lw	3, 0, 2
-	lw	4, 0, 3
-
-	lw	2, 0, 1
-	add	2, 2, 2
-	beq	2, 0, finish
-	beq	0, 0, inter2
+	lw	6, 0, 0
+	add	6, 6, 6
+	beq	6, 0, finish
+	beq	0, 0, loop1
 
 finish:	halt
