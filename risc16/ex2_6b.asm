0649aca870407b1685f75d4e744866afcc4757b8
nath@home <nathdwek@ulb.ac.be>
2016-02-26 23:09:54 +0100

    Check for end condition before shifting b

--- risc16/ex2_6b.asm
+++ risc16/ex2_6b.asm
@@ -10,7 +10,7 @@
 	addi	6, 0, 1 		//Running mask
 
 loop:	and	7, 1, 6			 //See if addition is needed
-	beq	7, 0, shftb		 // if masked r1 is 0, skip addition
+	beq	7, 0, pre		 // if masked r1 is 0, skip addition
 
 	add	3, 3, 2, carry 		//c [r3, r4] = c + b [r2, r5]
 	add	7, 0, 0
@@ -19,15 +19,16 @@ carry:	addi	7, 0, 1 		//In case of overflow, remember cy in r6
 adh:	add	4, 4, 5
 	add	4, 4, 7
 
-shftb:	add	2, 2, 2, shft1		//b [r2, r5] = b + b
+pre:	add	6, 6, 6, halt		//Shift the running mask. Overflow
+					//means we looped 16 times so stop
+					//in that case
+
+	add	2, 2, 2, shft1		//b [r2, r5] = b + b
 	addi	7, 0, 0
 	beq	0, 0, shftbh
 shft1: 	addi	7, 0, 1
 shftbh:	add	5, 5, 5
 	add	5, 5, 7
 
-	add	6, 6, 6, halt		//Shift the running mask. Overflow
-					//means we looped 16 times so stop
-					//in that case
 	beq	0, 0, loop
 halt:	halt
